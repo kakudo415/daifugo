1b249125f9d3412cae548c0b123bb68a9e1e671f
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-09-02 13:44:50 +0900

    feat: Implement echo server with WebSocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::get,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
@@ -7,6 +12,20 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<p>hello, world</p>")
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(game_server)
+}
+
+async fn game_server(mut ws: WebSocket) {
+    while let Some(Ok(msg)) = ws.recv().await {
+        if let Message::Text(msg) = msg {
+            if ws
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
 }
